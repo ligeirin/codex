4a1ab9553745c94ad82c672d61d378e8fe0ce590
= <=>
2023-09-19 21:14:50 -0300

    Add Filter Function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,18 @@
 let doc = document;
 
-function init() {
-	let entriesDiv = document.getElementById("entriesDiv");
-	console.log(entriesDiv);
+let ENTRIES_DATA = [];
 
-	ENTRIES.forEach((entry, i) => {
+function init() {
+	ENTRIES_DATA = JSON.parse(JSON.stringify(ENTRIES));
+	render();
+}
+
+function render() {
+	let entriesDiv = document.getElementById("entriesDiv");
+
+	entriesDiv.innerHTML = '';
+
+	ENTRIES_DATA.forEach((entry, i) => {
 		let entryWrapper = doc.createElement("div");
 		entryWrapper.id = "entry" + i;
 		entryWrapper.classList.add("entry");
@@ -49,9 +57,41 @@ function init() {
 
 		entryWrapper.onclick = (ev) => {
 			let id = ev.currentTarget.dataset.entry_id;
-			window.location.href = ENTRIES[id].path;
+			window.location.href = ENTRIES_DATA[id].path;
 		};
 
 		entriesDiv.append(entryWrapper);
 	});
+}
+
+
+function filterEntries(){
+	ENTRIES_DATA = ENTRIES;
+
+	let titleFilter = document.getElementById("titleFilterInput").value;
+		titleFilter = titleFilter ? titleFilter : null;
+
+	let tagFilter = document.getElementById("tagFilterInput").value;
+		tagFilter = tagFilter ? tagFilter.split(',') : null;
+
+	ENTRIES_DATA = ENTRIES_DATA.filter((entry) => {
+		if (titleFilter === null && tagFilter === null)
+			return true;
+
+		let titleCondition = titleFilter ? entry.name.toUpperCase().includes(titleFilter.toUpperCase()) : false;
+		let tagCondition = false;
+
+		if (tagFilter){
+			for (let tagF of tagFilter){
+				for (let tagDB of entry.tags){
+					if (tagF.toUpperCase() == TAGS[tagDB].name.toUpperCase())
+						tagCondition = true;
+				}
+			}
+		}
+
+		return tagCondition || titleCondition;
+	});
+
+	render();
 }
